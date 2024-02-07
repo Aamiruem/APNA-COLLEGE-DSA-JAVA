b4b126823a046eecd96b375d0e2b40ee45ab2b08
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-02-07 18:47:48 +0530

    commit changes

--- L12String/StringConcatenation.java
+++ L12String/StringConcatenation.java
@@ -1,12 +1,25 @@
+// import java.util.Scanner;
+
+// public class StringConcatenation {
+//     public static void main(String[] args) {
+//         // Concatenation
+//         String firstName = "tony";
+//         String lastName = "Stark";
+//         String fullName = firstName  + " " +   lastName;
+//         System.out.println(fullName);
+
+//     }
+// }
+
 import java.util.Scanner;
 
 public class StringConcatenation {
-    public static void main(String[] args) {
-        // Concatenation
-        String firstName = "tony";
-        String lastName = "Stark";
-        String fullName = firstName  + " " +   lastName;
-        System.out.println(fullName);
-        
-    }
+
+  public static void main(String[] args) {
+    // Concatenation
+    String firstName = "tony";
+    String lastName = "Stark";
+    String fullName = firstName + "@" + lastName;
+    System.out.println(fullName.length());
+  }
 }
