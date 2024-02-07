a2508e5ea8b08f1e64cb5cc720eb84296f827462
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-02-07 22:13:38 +0530

    commit change

--- L12String/CompareString.java
+++ L12String/CompareString.java
@@ -1,3 +1,40 @@
+// public class CompareString {
+
+//     public static void main(String[] args) {
+//     String name1 = "Tony";
+//     String name2 = "Tony";
+
+//     //1 s1>s2 : +ve value
+//     //2 s1 == s2 : 0 value
+//     //3 s1<s2 : -ve value
+
+//     if (name1.compareTo(name2) == 0) {
+//       System.out.println("Strings are equal");
+//     } else {
+//       System.out.println("Strings are not equal");
+//     }
+//   }
+// }
+
+// public class CompareString {
+
+//   public static void main(String[] args) {
+//     String name1 = "Tony1";
+//     String name2 = "Tony";
+
+//     //1 s1>s2 : +ve value
+//     //2 s1 == s2 : 0 value
+//     //3 s1<s2 : -ve value
+
+//     if (name1 == name2) {
+//       System.out.println("Strings are equal");
+//     } else {
+//       System.out.println("Strings are not equal");
+//     }
+//   }
+// }
+
+// kyu ki java ke undar string object hoti hai
 public class CompareString {
 
   public static void main(String[] args) {
@@ -8,7 +45,7 @@ public class CompareString {
     //2 s1 == s2 : 0 value
     //3 s1<s2 : -ve value
 
-    if (name1.compareTo(name2) == 0) {
+    if (new String("Tony") == new String("Tony")) {
       System.out.println("Strings are equal");
     } else {
       System.out.println("Strings are not equal");
---
+++ L12String/Substring.java
@@ -0,0 +1,23 @@
+// public class Substring {
+
+//   public static void main(String[] args) {
+//     // compare
+//     String sentence = "My name is Tony";
+//     String name = sentence.substring(11, sentence.length());
+//     System.out.println(name);
+//   }
+// }
+
+public class Substring {
+
+  public static void main(String[] args) {
+    // compare
+    String sentence = "Tony Stark";
+    String name1 = sentence.substring(0, 4);
+
+    String name2 = sentence.substring(5);
+    System.out.println(name1);
+    System.out.println(name2);
+  }
+}
+// Strings are Immutable
