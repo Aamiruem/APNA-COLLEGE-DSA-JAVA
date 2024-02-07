3a56e7edd49f1546d52ac2705fae4e4c1c089339
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-02-07 23:35:22 +0530

    commit changes

---
+++ com.company/L13String Builder/AddString.java
@@ -0,0 +1,12 @@
+public class AddString {
+
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder("H");
+
+    sb.append("e");
+    sb.append("l");
+    sb.append("l");
+    sb.append("o");
+    System.out.println(sb.length());
+  }
+}
